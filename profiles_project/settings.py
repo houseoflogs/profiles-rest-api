880fbafb9bce9dfdd15080bea8b6e61ea7495ffd
Travis Carney <public@netcaffeine.com>
2019-09-01 19:55:51 -0700

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '^hkixsr+i^dk#!8jar)y5*rp^*4i861_1rzq5rh&fqy1eeba@@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'communeapi.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
